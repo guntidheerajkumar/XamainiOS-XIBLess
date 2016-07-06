bed97953b9202249062588a0cd7607e34fbc978e
Dheeraj Kumar Gunti <guntidheerajkumar@gmail.com>
2016-07-06 16:12:33 +0530

    Working with UIViewController through Code
    Working with UIViewController through Code. We won't be using any
    designer. Everything is derived from Code.

---
+++ DemoXIBLess/FirstViewController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using MonoTouch.UIKit;
+using System.Drawing;
+
+namespace DemoXIBLess
+{
+	public class FirstViewController : UIViewController
+	{
+		public FirstViewController ()
+		{
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			this.View.BackgroundColor = UIColor.White;
+
+			var label = new UILabel (new RectangleF (10, 40, 300, 30));
+			label.Text = "Demo Application";
+			label.TextAlignment = UITextAlignment.Center;
+
+			this.View.AddSubview (label);
+		}
+	}
+}
+
